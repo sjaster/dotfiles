118fad19f503048c0bc3d94f78bb4a1755b0647a
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2017-12-10 00:17:47 +0100

    method used when update count in pacaur is false

--- polybar/pkg.sh
+++ polybar/pkg.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 pac=$(checkupdates | wc -l)
 aur=$(pacaur -u | wc -l)
+cower=$(cower -u | wc -l)
+
+if [[ $cower != $aur ]]
+then
+    aur=cower
+fi
 
 check=$((pac + aur))
 if [[ "$check" != "0" ]]

2e7232d68c2650af3d8c9c6d78db07800f26f42b
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2019-05-04 17:01:10 +0200

    rework polybar launch for multiple screens attached

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,9 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar top &
-polybar bottom &
-#polybar bar2 &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload top &
+  polybar --reload bottom &
+fi
 
 echo "Bars launched..."

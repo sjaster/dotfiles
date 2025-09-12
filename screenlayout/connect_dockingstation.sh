16c1488d4ca482de59eec012dbb51d80dd24ce06
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2025-09-12 13:54:34 +0200

    change clock style

--- screenlayout/connect_dockingstation.sh
+++ screenlayout/connect_dockingstation.sh
@@ -2,3 +2,4 @@
 xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP1-1 --off --output DP1-1-5 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1-1-6 --off --output DP1-2 --off --output DP1-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
 xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --off --output DP1-1-5 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1-1-6 --off --output DP1-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
 xinput set-button-map 15 1 2 3 4 5 6 7 0 0
+ln -s -f /home/sjaster/.dotfiles/alacritty/alacritty-dockingstation.toml /home/sjaster/.alacritty.toml

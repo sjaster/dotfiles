30427483c135853c3b06e0ea3165cfa279d73ae6
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2025-12-01 09:17:25 +0100

    update config

--- screenlayout/homeoffice.sh
+++ screenlayout/homeoffice.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1-1 --off --output DP1-1-5 --off --output DP1-1-6 --off --output DP1-2 --off --output DP1-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1-1 --off --output DP1-1-5 --off --output DP1-1-6 --off --output DP1-2 --off --output DP1-3 --off --output HDMI1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
